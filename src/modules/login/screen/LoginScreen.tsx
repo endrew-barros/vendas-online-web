7a60b990434349f9dbbb915e41108a247a43e5d0
endrew-barros <https://github.com/endrew-barros>
2024-09-14 20:24:43 -0400

    feat: crianimagem de login

---
+++ src/modules/login/screen/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import {BackgroundImage} from '../styles/loginScreenStyles'
+
+const LoginScreen = ()=>{
+  return(
+    <div>
+      <BackgroundImage src='./barraca.png' />
+    </div>
+  )
+}
+export default LoginScreen
